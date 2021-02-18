341c2f6b8bda9384a2b9b313f5e6da637708c3b2
Johnathan Nguyen <haungocvo@gmail.com>
2021-02-17 17:24:20 -0800

    ignore

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -4,30 +4,36 @@ import { FormControl } from '@angular/forms';
 import { Google } from '../google.model';
 import { GoogletranslateService } from '../services/googletranslate.service';
 
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
   lang = new FormControl('en');
-  translateObj: Google = new Google()
-  translated: string = ''
+  translateObj: Google = new Google();
+  translated: string = '';
+  isMobile: boolean;
 
-  constructor(private trans: GoogletranslateService,) { }
+  constructor(private trans: GoogletranslateService) {}
 
+  ngOnInit() {
+    if (window.screen.width < 768) {
+      this.isMobile = true;
+    } else {
+      this.isMobile = false;
+    }
+  }
   onTranslate(textInput: string, targetLanguage: string) {
-    this.translateObj.target = targetLanguage
-    this.translateObj.q = textInput
-    this.trans.translate(this.translateObj)
-      .subscribe((res: any) => {
-        this.translated = res.data.translations[0].translatedText
+    this.translateObj.target = targetLanguage;
+    this.translateObj.q = textInput;
+    this.trans.translate(this.translateObj).subscribe(
+      (res: any) => {
+        this.translated = res.data.translations[0].translatedText;
       },
-        err => {
-          console.log(err)
-        }
-      )
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
-
